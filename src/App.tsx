d0a031b71afcd4929d538bea9f70192f41a63beb
Gustav Tonér <gustav.toner@gmail.com>
2019-04-22 17:48:20 +0200

    Step 2

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,14 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.tsx</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="media">
+        <img className="mr-3" src="http://www.personalbrandingblog.com/wp-content/uploads/2017/08/blank-profile-picture-973460_640-300x300.png" style={{'width': '64px'}} alt="Generic placeholder" />
+        <div className="media-body">
+          <h5 className="mt-0">Username</h5>
+        </div>
       </div>
     );
   }
