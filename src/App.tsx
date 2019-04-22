cea6c60d9f423b119b9818ad9adb8576b5f03de9
Gustav Tonér <gustav.toner@gmail.com>
2019-04-22 18:01:19 +0200

    Step 3

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,24 @@
 import React, { Component } from 'react';
 import './App.css';
 
+const Card = (props: any) => {
+  return (
+    <div className="media">
+      <img className="mr-3" src={props.avatar_url} style={{'width': '64px'}} alt="Generic placeholder" />
+      <div className="media-body">
+        <h5 className="mt-0">{props.login}</h5>
+      </div>
+    </div>
+  );
+};
+
 class App extends Component {
   render() {
     return (
-      <div className="media">
-        <img className="mr-3" src="http://www.personalbrandingblog.com/wp-content/uploads/2017/08/blank-profile-picture-973460_640-300x300.png" style={{'width': '64px'}} alt="Generic placeholder" />
-        <div className="media-body">
-          <h5 className="mt-0">Username</h5>
-        </div>
-      </div>
+      <>
+        <Card login="Gustav" avatar_url="https://demos.subinsb.com/isl-profile-pic/image/person.png" />
+        <Card logi="Linnea" avatar_ur="https://demos.subinsb.com/isl-profile-pic/image/person.png" />
+      </>
     );
   }
 }
