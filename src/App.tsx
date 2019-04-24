3f6ff6ddcb0d9be45910306c98f2a55547da4918
Gustav Tonér <gustav.toner@gmail.com>
2019-04-24 07:17:12 +0200

    Switch to functional component

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,15 @@
 import React, { Component } from 'react';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="media">
+function App()  {
+  return (
+    <div className="media">
         <img className="mr-3" src="http://www.personalbrandingblog.com/wp-content/uploads/2017/08/blank-profile-picture-973460_640-300x300.png" style={{'width': '64px'}} alt="Generic placeholder" />
         <div className="media-body">
           <h5 className="mt-0">Username</h5>
         </div>
       </div>
-    );
-  }
+  );
 }
 
 export default App;
