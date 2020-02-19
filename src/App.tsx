0cd72b68ec4d2e05198a733d292412c9b5415fbd
Gustav Tonér <gustav.toner@gmail.com>
2020-02-19 20:38:56 +0100

    Update picture

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import './App.css';
 function App()  {
   return (
     <div className="media">
-        <img className="mr-3" src="http://www.personalbrandingblog.com/wp-content/uploads/2017/08/blank-profile-picture-973460_640-300x300.png" style={{'width': '64px'}} alt="Generic placeholder" />
+        <img className="mr-3" src="https://www.kemi.uu.se/digitalAssets/365/c_365694-l_3-k_no-picture.png" style={{'width': '64px'}} alt="Generic placeholder" />
         <div className="media-body">
           <h5 className="mt-0">Username</h5>
         </div>
